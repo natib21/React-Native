46eb7f478fb6c86cb365cfe74ed2214f6b5289b4
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-14 03:38:50 -0700

    improving the layout

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <TextInput placeholder='Your Cource Goal' style={styles.textInput}/>
         <Button title='Add Goal' />
       </View>
-      <View>
+      <View style={styles.goalsContainer} >
         <Text>List Of Goals...</Text>
       </View>
     </View>
@@ -17,19 +17,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer:{
-     padding:50,
+     paddingTop:50,
+     paddingHorizontal:16,
      borderWidth:2,
-     borderColor:'blue'
+     borderColor:'blue',
+     flex:1,
   },
   inputContainer:{
+    flex:1,
     flexDirection:'row',
     justifyContent:'space-between',
+    alignItems:'center',
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc'
   },
   textInput:{
     borderWidth:1,
     borderColor:'#cccccc',
-    width:'80%',
+    width:'70%',
     marginRight:8,
     padding:8
+  },
+  goalsContainer:{
+    flex:5
   }
 });
